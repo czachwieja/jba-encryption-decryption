9f7367169ca0e8f415c3a522237bab848c1890d7
Cezary Zachwieja <cezary@zachwieja.it>
2020-08-06 00:11:19 +0200

    Stage #1: Encrypted!

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,7 +1,23 @@
 package encryptdecrypt;
 
 public class Main {
+    private static final int SUM_OF_A_AND_Z_CHARACTERS = 'a' + 'z'; //219
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String message = "we found a treasure!";
+        String encryptedMessage = encrypt(message);
+        System.out.println(encryptedMessage);
+    }
+
+    private static String encrypt(String message) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            char character = message.charAt(i);
+            if (String.valueOf(character).matches("[a-z]")) {
+                stringBuilder.append((char) (SUM_OF_A_AND_Z_CHARACTERS - message.charAt(i)));
+            } else
+                stringBuilder.append(character);
+        }
+        return stringBuilder.toString();
     }
 }
