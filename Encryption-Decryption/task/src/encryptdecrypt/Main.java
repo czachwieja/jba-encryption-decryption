0f1396aeacd372d236c071b2fc40e9b206fcebcb
Cezary Zachwieja <cezary@zachwieja.it>
2020-08-06 00:56:20 +0200

    Stage #2: Knowledge is key

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,20 +1,25 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
-    private static final int SUM_OF_A_AND_Z_CHARACTERS = 'a' + 'z'; //219
+    private static final int CHARACTER_A_IN_ASCII = 'a'; //97
+    private static final int SUM_OF_CHARACTERS_FROM_A_TO_Z = 'z' + 1 - 'a'; //26
 
     public static void main(String[] args) {
-        String message = "we found a treasure!";
-        String encryptedMessage = encrypt(message);
+        Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+        int key = scanner.nextInt();
+        String encryptedMessage = encrypt(message, key);
         System.out.println(encryptedMessage);
     }
 
-    private static String encrypt(String message) {
+    private static String encrypt(String message, int key) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < message.length(); i++) {
             char character = message.charAt(i);
             if (String.valueOf(character).matches("[a-z]")) {
-                stringBuilder.append((char) (SUM_OF_A_AND_Z_CHARACTERS - message.charAt(i)));
+                stringBuilder.append((char) (((key + character - CHARACTER_A_IN_ASCII) % SUM_OF_CHARACTERS_FROM_A_TO_Z) + CHARACTER_A_IN_ASCII));
             } else
                 stringBuilder.append(character);
         }
