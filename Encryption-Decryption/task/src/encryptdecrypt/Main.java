5efda0714d4931a887fc8b66492624ba1e92e6ad
Cezary Zachwieja <cezary@zachwieja.it>
2020-08-08 00:26:51 +0200

    Stage #3: Decrypted!

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -3,14 +3,20 @@ package encryptdecrypt;
 import java.util.Scanner;
 
 public class Main {
-    private static final int CHARACTER_A_IN_ASCII = 'a'; //97
-    private static final int SUM_OF_CHARACTERS_FROM_A_TO_Z = 'z' + 1 - 'a'; //26
+    private static final int CHARACTER_SPACE_IN_ASCII = ' '; // 32
+    private static final int SUM_OF_CHARACTERS_FROM_SPACE_TO_TILDE = '~' + 1 - ' '; // 95 = 126 + 1 - 32
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String operation = scanner.nextLine();
         String message = scanner.nextLine();
         int key = scanner.nextInt();
-        String encryptedMessage = encrypt(message, key);
+        String encryptedMessage = "";
+        if ("enc".equals(operation)) {
+            encryptedMessage = encrypt(message, key);
+        } else if ("dec".equals(operation)) {
+            encryptedMessage = decrypt(message, key);
+        }
         System.out.println(encryptedMessage);
     }
 
@@ -18,11 +24,18 @@ public class Main {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < message.length(); i++) {
             char character = message.charAt(i);
-            if (String.valueOf(character).matches("[a-z]")) {
-                stringBuilder.append((char) (((key + character - CHARACTER_A_IN_ASCII) % SUM_OF_CHARACTERS_FROM_A_TO_Z) + CHARACTER_A_IN_ASCII));
-            } else
-                stringBuilder.append(character);
+            stringBuilder.append((char) (((character + key - CHARACTER_SPACE_IN_ASCII) % SUM_OF_CHARACTERS_FROM_SPACE_TO_TILDE) + CHARACTER_SPACE_IN_ASCII));
         }
         return stringBuilder.toString();
     }
+
+    private static String decrypt(String message, int key) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            char character = message.charAt(i);
+            stringBuilder.append((char) (((character - key - CHARACTER_SPACE_IN_ASCII) % SUM_OF_CHARACTERS_FROM_SPACE_TO_TILDE) + CHARACTER_SPACE_IN_ASCII));
+        }
+        return stringBuilder.toString();
+    }
+
 }
